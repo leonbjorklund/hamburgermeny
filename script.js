02b27365bbc499cf838d6d5fcef1cc6461670a48
Leon Björklund <leon.bjorklund@gmail.com>
2022-11-11 15:05:45 +0100

    updated some shit

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function expandHamburger () {
     let element = document.getElementById("expanding");
     element.classList.toggle("make-100vh");
     
+    let hide = document.getElementsByClassName("about")[0].style.display = "inline-block";
+    hide.classList.toggle("hide-about");
 }
